1758f140e07578f1a939dc64f61835e41b240e22
AbdelrahmanMohamedSalem <57458231+AbdelrahmanMohamedSalem@users.noreply.github.com>
2019-11-14 14:31:06 +0200

    third commit

---
+++ ConstructionSystem/Models/Entities/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ConstructionSystem.Models.Entities
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        [Required]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Must Be Between 2 & 50 Character")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Must Be Between 2 & 50 Character")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+
+        public string Image { get; set; }
+
+        [StringLength(15, MinimumLength = 11, ErrorMessage = "Please Enter a Valid Phone Number")]
+        public string Phone { get; set; }
+
+        public decimal? Salary { get; set; }
+
+        public bool? Gender { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Birth Date")]
+        public DateTime? BirthDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Hire Date")]
+        public DateTime? HireDate { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        public Employee employee { get; set; }
+
+        [ForeignKey("employee")]
+        [Display(Name = "Supervisor")]
+        public int? SuperId { get; set; }
+
+        public Department Department { get; set; }
+
+        [ForeignKey("Department")]
+        [Display(Name = "Department")]
+        public int? DepartmentID { get; set; }
+
+    }
+}
